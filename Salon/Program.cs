d59e016d477c0ed96636874bd1041eccfb480f16
brnydr <brianwilliamyoder@gmail.com>
2023-10-05 08:58:11 -0700

    edit program.cs

--- Salon/Program.cs
+++ Salon/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Salon.Models
+using Salon.Models;
 
 namespace Salon
 {

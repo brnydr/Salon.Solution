170f0e9245fe6ac38158da2ba9ce8fe5a358d4ee
brnydr <brianwilliamyoder@gmail.com>
2023-10-05 10:17:12 -0700

    add create view and routes

--- Salon/Controllers/ClientsController.cs
+++ Salon/Controllers/ClientsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace Salon.Controllers
 {
@@ -18,8 +19,28 @@ namespace Salon.Controllers
     public ActionResult Index()
     {
       ViewBag.PageTitle = "All Clients";
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients
+        .Include(client => client.Stylist)
+        .ToList();
       return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add a Client";
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
